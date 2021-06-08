54a762cc006d3513bdfdecd6b0d320432590538c
DESKTOP-GBM58UN\ctran <cathyht@uci.edu>
2021-06-08 14:30:07 -0700

    debug timer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -411,7 +411,11 @@ socket.on('game_update', (payload) =>{
             let seconds = Math.floor((elapsed_m % (60 * 1000)) / 1000);
             let total = minutes * 60 + seconds;
             if (total > 100) {
-                total = 100;
+                total = 100; /* cap progress bar to 100 */
+             }
+             if (total < 0) {
+                total = 0;
+                last_time = d.getTime();
              }
              $("#elapsed").css("width", total + "%").attr("aria-valuenow", total);
             let timestring = "" + seconds;
